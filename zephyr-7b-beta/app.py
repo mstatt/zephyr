6fc986fa99ae3a5a2bc6620644e9e07376bd48e9
mstatt <mike@supercess.com>
2023-12-02 03:44:50 +0400

    fixed typo

--- zephyr-7b-beta/app.py
+++ zephyr-7b-beta/app.py
@@ -18,8 +18,12 @@ def get_out_text(text):
 # Load model directly
 	# Use a pipeline as a high-level helper
 	from transformers import pipeline
-	print(text)
-	pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta")
+	pipe = pipeline("text-generation", model="HuggingFaceH4/zephyr-7b-beta",
+		temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+		top_p=0.15,  # select from top tokens whose probability add up to 15%
+		top_k=0,  # select from top 0 tokens (because zero, relies on top_p)
+		max_new_tokens=1024,  # mex number of tokens to generate in the output
+		repetition_penalty=1.1)
 	final_out = str(pipe(text))
 	print(final_out)
 
